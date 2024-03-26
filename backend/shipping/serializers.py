82c446868719e58d855bd1f38488863771143c19
Alex <ingaksdr@gmail.com>
2024-03-26 21:18:30 +0300

    Add serializer

---
+++ backend/shipping/serializers.py
@@ -0,0 +1,69 @@
+from rest_framework import serializers
+from .models import Location, Truck, Cargo
+from .distance import calculate_distance
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+class TruckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Truck
+        fields = '__all__'
+
+class CargoCreateSerializer(serializers.ModelSerializer):
+    pick_up_zip = serializers.CharField(max_length=10)
+    delivery_zip = serializers.CharField(max_length=10)
+
+    class Meta:
+        model = Cargo
+        fields = ['pick_up_zip', 'delivery_zip', 'weight', 'description']
+
+    def create(self, validated_data):
+        pick_up_zip = validated_data.pop('pick_up_zip')
+        delivery_zip = validated_data.pop('delivery_zip')
+
+        # Геокодируем zip-коды для определения координат
+        pick_up_location = Location.objects.filter(zip_code=pick_up_zip).first()
+        delivery_location = Location.objects.filter(zip_code=delivery_zip).first()
+
+        if not pick_up_location or not delivery_location:
+            raise serializers.ValidationError("Invalid zip code(s)")
+
+        validated_data['pick_up_location'] = pick_up_location
+        validated_data['delivery_location'] = delivery_location
+
+        return super().create(validated_data)
+
+
+class CargoListSerializer(serializers.ModelSerializer):
+    pick_up_location = serializers.SerializerMethodField()
+    delivery_location = serializers.SerializerMethodField()
+    nearby_trucks_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cargo
+        fields = ['id', 'pick_up_location', 'delivery_location', 'nearby_trucks']
+
+    def get_pick_up_location(self, obj):
+        return obj.pick_up_location.city
+
+    def get_delivery_location(self, obj):
+        return obj.delivery_location.city
+
+    def get_nearby_trucks(self, obj):
+        cargo_pickup_location = (obj.pick_up_location.latitude, obj.pick_up_location.longitude)
+        nearby_trucks = []
+        for truck in Truck.objects.all():
+            truck_location = (truck.current_location.latitude, truck.current_location.longitude)
+            distance = calculate_distance(truck_location, cargo_pickup_location)
+            if distance <= 450:
+                nearby_trucks.append({
+                    'id': truck.id,
+                    'current_location': {
+                        'latitude': truck.current_location.latitude,
+                        'longitude': truck.current_location.longitude
+                    }
+                })
+        return nearby_trucks
--- backend/shipping/views.py
+++ backend/shipping/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Cargo
+from .serializers import CargoListSerializer
 
-# Create your views here.
+class CargoViewSet(viewsets.ModelViewSet):
+    queryset = Cargo.objects.all()
+    serializer_class = CargoListSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
